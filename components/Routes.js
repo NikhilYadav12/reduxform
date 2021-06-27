95e635d0a7a3f3e53221ed8a8c6638177b153626
NikhilYadav12 <86563702+NikhilYadav12@users.noreply.github.com>
2021-06-27 20:45:52 +0530

    Add files via upload

---
+++ components/Routes.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignInScreen from "./signup";
+import SignupScreen from "./page2";
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer headerMode="false">
+      <Stack.Navigator
+      
+      >
+        <Stack.Screen name="Login" component={SignInScreen} />
+
+        <Stack.Screen name="Signup" component={SignupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
